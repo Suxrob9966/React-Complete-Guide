7f95435d79bcfe0786544344cdb36f43961df73c
Suxrob9966 <rabbimovich9966@gmail.com>
2021-08-04 10:33:39 -0500

    first custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = function () {
+  return (
+    <div>
+      <div>August 4th 2021</div>
+      <div>
+        <h2>Tuition fee</h2>
+        <div>$3880.00</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
