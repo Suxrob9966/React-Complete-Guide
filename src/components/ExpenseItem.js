44cb081d52c7514e9d4db8f352c1b2d5287d2402
Suxrob9966 <rabbimovich9966@gmail.com>
2021-08-04 12:57:35 -0500

    props concept

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = function () {
-  const expenseDate = new Date(2021, 6, 4);
-  const expenseTitle = "Tuition fee";
-  const expenseAmount = 3880.0;
-
+const ExpenseItem = function (props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">$ {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
