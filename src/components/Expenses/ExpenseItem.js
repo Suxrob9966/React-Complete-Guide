250c69a55d2197cc72d08d433e6688813ddd87a2
Suxrob9966 <rabbimovich9966@gmail.com>
2021-08-05 13:33:21 -0500

    using state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,20 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React from "react";
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => console.log("Clicked!!!");
+  const [title, setTitle] = useState(props.title); // we pass the element to be changed as a parameter into useState() function. This function always returns an array where first element is the current state value and the second element is the function for updating that element
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
