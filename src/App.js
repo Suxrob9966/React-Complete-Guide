4908b79b4441e740db7fa6b208a637d95324ff20
Suxrob9966 <rabbimovich9966@gmail.com>
2021-08-09 17:35:04 -0500

    adding chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import Chart from "./components/Chart/Chart";
 import React, { useState } from "react";
 
 const dummyExpenses = [
@@ -48,7 +49,7 @@ const App = () => {
       <NewExpense onAddExpense={addExpenseHandler} />
 
       {/*we pass expenses array elements as attributes into custom defined element ExpenseItem. the attribute names can be anything. But in ExpenseItem.js file we will have to access these attributes by these names. In that file these attributes will be stored as function parameter called props. We then acccess every attribute element as props[0].title for example.*/}
-
+      <Chart />
       <Expenses items={expenses}></Expenses>
     </div>
   );
